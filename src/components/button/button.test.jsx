a09324de4294643a3481dbcc3b3caccc47a1ac5d
Joshua Crass <joshua@ematix.com>
2017-11-03 13:48:45 +0100

    fixed testing

--- src/components/button/button.test.jsx
+++ src/components/button/button.test.jsx
@@ -1,25 +1,9 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { shallow } from "enzyme";
 
 import Button from "./index";
 
 describe("<Button />", () => {
-  const component = shallow(<Button />);
-
-  it("renders 1 button component", () => {
-    expect(component).toHaveLength(1);
-  });
-
-  it("is of type button", () => {
-    expect(component.type()).toBe("button");
-  });
-
-  it("has props", () => {
-    expect(component.instance().props.darkBg).toBe(true);
-    expect(component.instance().props.primary).toBe(true);
-  });
-
   it("renders correctly", () => {
     const tree = renderer
       .create(<div>
@@ -29,7 +13,7 @@ describe("<Button />", () => {
         <div>
           <Button primary>Secondary</Button>
         </div>
-              </div>)
+      </div>)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
