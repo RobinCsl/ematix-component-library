009d770fadda63de8bef9409b8c1d6afaeb03d22
Joshua Crass <joshua@ematix.com>
2017-11-27 15:36:28 +0100

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/components/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        include: path.resolve(__dirname, "src"),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env", "react", "stage-0"],
+          },
+        },
+      },
+      {
+        test: /\.(s)?css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              includePaths: ["node_modules"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    modules: ["src", "node_modules"],
+    extensions: [".js", ".jsx"],
+  },
+  externals: {
+    react: "commonjs react",
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+      compressor: {
+        drop_console: true,
+        warnings: false,
+      },
+    }),
+  ],
+};
